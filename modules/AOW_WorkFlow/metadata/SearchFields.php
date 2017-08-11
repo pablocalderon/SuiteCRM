9efd87d7d4618fe6e6cbef38d4198886c128605a
Dillon Brown <dillon.brown@salesagility.com>
2017-08-11 14:31:55 +0100

    temp commit

--- modules/AOW_WorkFlow/metadata/SearchFields.php
+++ modules/AOW_WorkFlow/metadata/SearchFields.php
@@ -29,6 +29,8 @@ $searchFields['AOW_WorkFlow'] =
 		'name' => array( 'query_type'=>'default'),
 		'current_user_only'=> array('query_type'=>'default','db_field'=>array('assigned_user_id'),'my_items'=>true, 'vname' => 'LBL_CURRENT_USER_FILTER', 'type' => 'bool'),
 		'assigned_user_id'=> array('query_type'=>'default'),
+        'module' => array('query_type'=>'default'),
+
 	    
 		//Range Search Support 
 	   'range_date_entered' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
@@ -37,6 +39,6 @@ $searchFields['AOW_WorkFlow'] =
 	   'range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
 	   'start_range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
        'end_range_date_modified' => array ('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),	
-	    //Range Search Support 		
+	    //Range Search Support
 	);
-?>
+
