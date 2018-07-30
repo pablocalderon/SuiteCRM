fd00a6fd5f5dd0dcd0689a77e0b0554e512ed465
Dillon-Brown <dillon.brown@salesagility.com>
2018-07-30 11:06:25 +0100

    Cancel.php

--- modules/Ews/Create.php
+++ modules/Ews/Create.php
@@ -1,6 +1,49 @@
 <?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/Exchange.php';
 
 use jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfAllItemsType;
 use jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfAttendeesType;
@@ -23,8 +66,10 @@ class Create extends SugarBean
 
     public function createMeeting(SugarBean $bean, User $user)
     {
+        $exchange = new Exchange();
+
         $guests = $this->getAttendees();
-        $client = $this->createClient($user);
+        $client = $exchange->setConnection($user);
         $request = $this->buildRequest();
         $event = $this->buildEvent($bean);
         $this->setBody($event);
@@ -123,18 +168,6 @@ class Create extends SugarBean
             }
         }
         return $guests;
-    }
-
-    protected function createClient(User $user)
-    {
-        $username = $user->email1;
-        $password = 'salesagilitypassword1';
-        $host = 'outlook.com';
-        $version = Client::VERSION_2016;
-
-        $client = new Client($host, $username, $password, $version);
-
-        return $client;
     }
 
     protected function buildRequest()
