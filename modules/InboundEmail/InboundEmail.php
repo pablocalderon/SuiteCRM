7230fc553548901261eed3ba83221c66f4a11007
Dillon-Brown <dillon.brown@salesagility.com>
2019-02-18 11:10:00 +0000

    Fixed #6470 - Email module: Inline image not shown in received/sent email

--- modules/InboundEmail/InboundEmail.php
+++ modules/InboundEmail/InboundEmail.php
@@ -3970,6 +3970,8 @@ class InboundEmail extends SugarBean
     {
         global $sugar_config;
 
+        $cacheDir = $GLOBALS['sugar_config']['cache_dir'] . 'images/';
+
         $msgPart = '';
         $bc = $this->buildBreadCrumbs($structure->parts, $type);
         //Add an offset if specified
@@ -3997,9 +3999,8 @@ class InboundEmail extends SugarBean
                                 continue;
                             }
                             $partid = substr($part->id, 1, -1); // strip <> around
-                            if (isset($this->inlineImages[$partid])) {
-                                $imageName = $this->inlineImages[$partid];
-                                $newImagePath = "class=\"image\" src=\"{$this->imagePrefix}{$imageName}\"";
+                            if ($part->parameters[0]->value) {
+                                $newImagePath = "class=\"image\" src=\"{$cacheDir}{$this->inlineImages[$partid]}\"";
                                 $preImagePath = "src=\"cid:$partid\"";
                                 $msgPartRaw = str_replace($preImagePath, $newImagePath, $msgPartRaw);
                             }
@@ -4583,6 +4584,7 @@ class InboundEmail extends SugarBean
         }
 
         $uploadDir = ($forDisplay) ? "{$this->EmailCachePath}/{$this->id}/attachments/" : "upload://";
+        $cacheDir = $GLOBALS['sugar_config']['cache_dir'] . 'images';
 
         // decide what name to save file as
         $fileName = htmlspecialchars($attach->id);
@@ -4611,6 +4613,11 @@ class InboundEmail extends SugarBean
         if ((strtolower($part->disposition) == 'inline' && in_array($part->subtype, $this->imageTypes))
             || ($part->type == 5)
         ) {
+
+            if (!is_dir($cacheDir)) {
+                mkdir($cacheDir);
+            }
+
             if (copy($uploadDir . $fileName, sugar_cached("images/{$fileName}.") . strtolower($part->subtype))) {
                 $id = substr($part->id, 1, -1); //strip <> around
                 $this->inlineImages[$id] = $attach->id . "." . strtolower($part->subtype);
